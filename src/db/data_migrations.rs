74826eb1e2001245bea9623bcb08aaa65a624e2e
projectmoon <projectmoon@agnos.is>
2020-10-30 21:48:52 +0000

    Add string name to migrations

--- src/db/data_migrations.rs
+++ src/db/data_migrations.rs
@@ -1,12 +1,12 @@
 use crate::db::errors::{DataError, MigrationError};
-use crate::db::variables;
+use crate::db::variables::migrations::*;
 use crate::db::Database;
 use phf::phf_map;
 
-pub(super) type DataMigration = fn(&Database) -> Result<(), DataError>;
+pub(super) type DataMigration = (fn(&Database) -> Result<(), DataError>, &'static str);
 
 static MIGRATIONS: phf::Map<u32, DataMigration> = phf_map! {
-    1u32 => variables::migrations::add_room_user_variable_count,
+    1u32 => (add_room_user_variable_count, "add_room_user_variable_count"),
 };
 
 pub fn get_migrations(versions: &[u32]) -> Result<Vec<DataMigration>, MigrationError> {
