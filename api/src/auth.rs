d94451897cdd25a70f1a69269c6821825470cecc
projectmoon <projectmoon@agnos.is>
2021-06-09 15:41:57 +0000

    Also create a refresh token, and store it as HTTP only cookie.

--- api/src/auth.rs
+++ api/src/auth.rs
@@ -1,6 +1,7 @@
 use crate::config::Config;
 use chrono::{Duration, Utc};
 use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+use rocket::http::{Cookie, CookieJar};
 use rocket::response::status::Custom;
 use rocket::{
     http::Status,
@@ -25,27 +26,40 @@ struct LoginRequest<'a> {
     password: &'a str,
 }
 
-#[rocket::post("/login", data = "<request>")]
-async fn login<'a>(
-    request: Json<LoginRequest<'a>>,
-    config: &State<Config>,
+fn create_token<'a>(
+    username: &str,
+    expiration: Duration,
+    secret: &str,
 ) -> Result<String, Custom<String>> {
     let expiration = Utc::now()
-        .checked_add_signed(Duration::seconds(60))
+        .checked_add_signed(expiration)
         .expect("clock went awry")
         .timestamp();
 
     let claims = Claims {
         exp: expiration as usize,
-        sub: request.username.to_owned(),
+        sub: username.to_owned(),
     };
 
     let token = encode(
         &Header::default(),
         &claims,
-        &EncodingKey::from_secret(config.jwt_secret.as_ref()),
+        &EncodingKey::from_secret(secret.as_bytes()),
     )
     .map_err(|e| Custom(Status::InternalServerError, e.to_string()))?;
 
+    Ok(token)
+}
+
+#[rocket::post("/login", data = "<request>")]
+async fn login(
+    request: Json<LoginRequest<'_>>,
+    config: &State<Config>,
+    cookies: &CookieJar<'_>,
+) -> Result<String, Custom<String>> {
+    let token = create_token(request.username, Duration::minutes(1), &config.jwt_secret)?;
+    let refresh_token = create_token(request.username, Duration::weeks(1), &config.jwt_secret)?;
+
+    cookies.add_private(Cookie::new("refresh_token", refresh_token));
     Ok(token)
 }
