258f7f8b888f0544c8c134b3a3b2c6cd3d3690c5
projectmoon <projectmoon@agnos.is>
2021-06-12 23:00:58 +0000

    Clean up and document index.js file.

--- web-ui/index.js
+++ web-ui/index.js
@@ -1,6 +1,6 @@
 //The wasm application is compiled as javascript into the /pkg
 //directory. Webpack then replaces this import with what is actually
-//needed.
+//needed. To import the web assembly, the import FUNCTION must be
+//used. The import STATEMENT does not work.
 import './webui.scss';
-//import('webui.scss');
-import("./pkg");
+import('./pkg');
